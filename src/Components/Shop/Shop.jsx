cb55d0ffb5eb8a24fb6aa0c6e8d2a4b66182a05b
programmerrafi <mdrafiprogrammer@gmail.com>
2024-09-08 00:04:34 +0600

    new-update

--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -1,94 +1,99 @@
-import { useEffect, useState } from 'react'
-import ShopItem from './Shopitem'
-import { Bars } from 'react-loader-spinner'
-import { useDispatch, useSelector } from 'react-redux'
-import { addItem, selectItems } from "../../redux/slices/basketSlice"
- 
+import { useEffect, useState } from "react";
+import ShopItem from "./Shopitem";
+import { Bars } from "react-loader-spinner";
+import { useDispatch, useSelector } from "react-redux";
+import { addItem, selectItems } from "../../redux/slices/basketSlice";
+
 const Shop = () => {
-  const [products, setProducts] = useState([])
-  const [loading, setLoading] = useState(false)
-  const [loadedProducts, setLoadedProducts] = useState([])
-  const dispatch = useDispatch()
-  const cartItems = useSelector(selectItems)
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [loadedProducts, setLoadedProducts] = useState([]);
+  const dispatch = useDispatch();
+  const cartItems = useSelector(selectItems);
 
   useEffect(() => {
-    (
-      async () => {
-        setLoading(true)
-        const response = await fetch('https://course-api.com/react-store-products', {
-          mode: 'cors',
-          headers: {
-            'Access-Control-Allow-Origin':'*'
-          }
-        }).then(res => res.json()).catch(err => console.log("ERROR: ", err)).finally(() => setLoading(false))
-        setProducts(response)
-        setLoadedProducts(createPagination(response))
-      }
-    )();
+    (async () => {
+      setLoading(true);
+      const response = await fetch(
+        "https://www.course-api.com/react-store-products"
+      )
+        .then((res) => res.json())
+        .catch((err) => console.log("ERROR: ", err))
+        .finally(() => setLoading(false));
+      setProducts(response);
+      setLoadedProducts(createPagination(response));
+    })();
   }, []);
 
   const createPagination = (items, limit = 8, offset = 0) => {
-    let arr = []
+    let arr = [];
     items.forEach((item, index) => {
       if (index >= offset && index < offset + limit) {
-        arr.push(item)
+        arr.push(item);
       }
-    })
-    return arr
-  }
+    });
+    return arr;
+  };
 
   const loadMore = () => {
-    if (loadedProducts.length === products.length) return
-    const newProducts = createPagination(products, 8, loadedProducts.length)
-    setLoadedProducts([...loadedProducts, ...newProducts])
-  }
+    if (loadedProducts.length === products.length) return;
+    const newProducts = createPagination(products, 8, loadedProducts.length);
+    setLoadedProducts([...loadedProducts, ...newProducts]);
+  };
 
   const addToBasket = (product) => {
-    dispatch(addItem({
-      ...product,
-      quantity: 1
-    }))
-  }
-  
+    dispatch(
+      addItem({
+        ...product,
+        quantity: 1,
+      })
+    );
+  };
+
   const checkItemExists = (id) => {
-    const find = cartItems.filter(item => item.id === id)
-    return !!find.length
-  }
+    const find = cartItems.filter((item) => item.id === id);
+    return !!find.length;
+  };
 
   return (
     <>
-      <section className='block h-auto'>
+      <section className="block h-auto">
         <div className="container md:w-5/6 mx-auto px-2 md:px-0">
-          <div className='grid md:grid-cols-2 grid-cols-1 justify-center items-center gap-9 my-12'>
-            <h2 className='text-5xl font-bold leading-tight '>Shop by Product</h2>
+          <div className="grid md:grid-cols-2 grid-cols-1 justify-center items-center gap-9 my-12">
+            <h2 className="text-5xl font-bold leading-tight ">
+              Shop by Product
+            </h2>
           </div>
           <div>
-            <div className='flex flex-wrap'>
+            <div className="flex flex-wrap">
               {loading && (
                 <div className="flex justify-center w-full my-10">
-                  <Bars
-                    color='#E1C8B4'
-                    ariaLabel='loading'
-                  />
+                  <Bars color="#E1C8B4" ariaLabel="loading" />
                 </div>
               )}
-              {!!loadedProducts.length && loadedProducts?.map(product => (
-                <ShopItem
-                  key={product?.id}
-                  {...product}
-                  onClick={() => addToBasket(product)}
-                  exists={checkItemExists}
-                />
-              ))}
+              {!!loadedProducts.length &&
+                loadedProducts?.map((product) => (
+                  <ShopItem
+                    key={product?.id}
+                    {...product}
+                    onClick={() => addToBasket(product)}
+                    exists={checkItemExists}
+                  />
+                ))}
             </div>
             {loadedProducts.length !== products.length && (
-              <button onClick={loadMore} className="bg-black mx-auto text-white px-11 py-3 block">Load More</button>
+              <button
+                onClick={loadMore}
+                className="bg-black mx-auto text-white px-11 py-3 block"
+              >
+                Load More
+              </button>
             )}
           </div>
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Shop
+export default Shop;
